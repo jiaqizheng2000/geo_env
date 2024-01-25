cdd11e2549efe9cf56136c7d0bf7ccb26a355ab6
zhengj <jiaqi.zheng@kaust.edu.sa>
2024-01-25 10:35:47 +0300

    Added code to visualize Jeddah's topography.

--- assignment_1.py
+++ assignment_1.py
@@ -9,10 +9,12 @@ import xarray as xr
 dset = xr.open_dataset("SRTMGL1_NC.003_-Data/N21E039.SRTMGL1_NC.nc")
 
 # allow examination of variables and objects in the memory
-pdb.set_trace()
+# pdb.set_trace()
 
 # use dest and dset.variables to explore the variables in the file
 DEM = np.array(dset.variables['SRTMGL1_DEM'])
+
+pdb.set_trace()
 
 # close the netCDF dile
 dset.close()
